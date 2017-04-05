1b4cca7cd83bf03934c910550cbf830fb2d5622b
Noel Parisi <ycleptnoel@gmail.com>
2017-04-04 18:17:30 -0700

    retrieve last 20 tweets

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
+// console.log('this is loaded');
+var Twitter = require('twitter');
+exports.twitterKeys = new Twitter ({
   consumer_key: 'sT9NK1h6Tpgqtabt2OUFfrLgn',
   consumer_secret: 'TsuL7O4LJOKcWxaX1NA23r4N1uv8r2MPSUXEMBLwqsbNTtaXV9',
   access_token_key: '607481925-tcnBiP6kl8D8jRr2yhcCr6OzqCUd88LyWW9eX9sB',
   access_token_secret: 'LS7kWiCyFVzd4LZBYm7xYeyFS5MNqiWHA5Es2OeDBRzzz',
-}
+});
--- liri.js
+++ liri.js
@@ -1,35 +1,47 @@
 // my-tweets
-	// This will show your last 20 tweets 
-	//and when they were created at in your terminal/bash window.
+// This will show your last 20 tweets 
+//and when they were created at in your terminal/bash window.
+const twitter = require('twitter');
 const keys = require('./keys.js');
-const twitterAccess = keys.twitterKeys
+const twitterAccess = keys.twitterKeys;
+// console.log(twitterAccess);
+if (process.argv[2] === "my_tweets") {
+    var params = { screen_name: 'noelannparisi' };
+    twitterAccess.get('statuses/user_timeline', 'count:20', function(error, tweets, response) {
+        if (!error) {
+            for (tweet in tweets) {
+                console.log("You tweeted:", tweets[tweet].text, " on", tweets[tweet].created_at + "\n\n");
+            }
+        }
+    });
+}
 
 // spotify-this-song
-	//This will show the following information about the song in your terminal/bash window
-	//Artist(s)
-	//The song's name
-	//A preview link of the song from Spotify
-	//The album that the song is from
-	//if no song is provided then your program will default to
-	//"The Sign" by Ace of Base
+//This will show the following information about the song in your terminal/bash window
+//Artist(s)
+//The song's name
+//A preview link of the song from Spotify
+//The album that the song is from
+//if no song is provided then your program will default to
+//"The Sign" by Ace of Base
 
 // movie-this
-	//This will output the following information to your terminal/bash window:
-	//   * Title of the movie.
-	//   * Year the movie came out.
-	//   * IMDB Rating of the movie.
-	//   * Country where the movie was produced.
-	//   * Language of the movie.
-	//   * Plot of the movie.
-	//   * Actors in the movie.
-	//   * Rotten Tomatoes Rating.
-	//   * Rotten Tomatoes URL.
-	// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-	// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-	// It's on Netflix!
+//This will output the following information to your terminal/bash window:
+//   * Title of the movie.
+//   * Year the movie came out.
+//   * IMDB Rating of the movie.
+//   * Country where the movie was produced.
+//   * Language of the movie.
+//   * Plot of the movie.
+//   * Actors in the movie.
+//   * Rotten Tomatoes Rating.
+//   * Rotten Tomatoes URL.
+// If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
+// It's on Netflix!
 
 // do-what-it-says
-	//Using the fs Node package, LIRI will take the text inside of random.txt and then use it 
-	//to call one of LIRI's commands.
-	//It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-	//Feel free to change the text in that document to test out the feature for other commands.
+//Using the fs Node package, LIRI will take the text inside of random.txt and then use it 
+//to call one of LIRI's commands.
+//It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+//Feel free to change the text in that document to test out the feature for other commands.
