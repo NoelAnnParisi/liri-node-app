bcbfd4f27c6ac4ccb947679fd129ee13d871af8d
Noel Parisi <ycleptnoel@gmail.com>
2017-04-04 18:37:42 -0700

    spotify beginnings

--- liri.js
+++ liri.js
@@ -2,19 +2,41 @@
 // This will show your last 20 tweets 
 //and when they were created at in your terminal/bash window.
 const twitter = require('twitter');
+const spotify = require('spotify');
 const keys = require('./keys.js');
 const twitterAccess = keys.twitterKeys;
+const input = process.argv[2];
 // console.log(twitterAccess);
-if (process.argv[2] === "my_tweets") {
-    var params = { screen_name: 'noelannparisi' };
-    twitterAccess.get('statuses/user_timeline', 'count:20', function(error, tweets, response) {
-        if (!error) {
-            for (tweet in tweets) {
-                console.log("You tweeted:", tweets[tweet].text, " on", tweets[tweet].created_at + "\n\n");
+
+switch (input) {
+    case "my_tweets":
+        // var params = { screen_name: 'noelannparisi' };
+        // twitterAccess.get('statuses/user_timeline', 'count:20', function(error, tweets, response) {
+        //     if (!error) {
+        //         for (tweet in tweets) {
+        //             console.log("You tweeted:", tweets[tweet].text, " on", tweets[tweet].created_at + "\n\n");
+        //         }
+        //     }
+        // });
+        break;
+    case "spotify-this-song":
+        // node liri.js spotify-this-song[2] '<song name here>'[3]
+        // for (let i = 2; i < process.argv.length; i ++){
+        // }
+        const songName = process.argv[3];
+        // try getting rid of the quotes!
+        //try doing the for loop!
+        // read the docs! YOU GOT THIS
+        console.log(songName);
+        spotify.search({ type: 'track', query: songName }, function(err, data) {
+            if (!err) {
+                console.log('Error occurred: ' + err);
+                return;
             }
-        }
-    });
+        });
+        break;
 }
+
 
 // spotify-this-song
 //This will show the following information about the song in your terminal/bash window
