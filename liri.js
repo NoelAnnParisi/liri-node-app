852c4a1affcebdb3558b574716cade27827b49b8
Noel Parisi <ycleptnoel@gmail.com>
2017-04-08 10:17:46 -0700

    finishing up

--- liri.js
+++ liri.js
@@ -5,16 +5,17 @@ const fs = require('fs');
 const keys = require('./keys.js');
 const twitterAccess = keys.twitterKeys;
 const input = process.argv[2];
+
 switch (input) {
-    // my-tweets
-    // This will show your last 20 tweets 
-    //and when they were created at in your terminal/bash window.
     case "my_tweets":
         var params = { screen_name: 'noelannparisi' };
         twitterAccess.get('statuses/user_timeline', 'count:20', function(error, tweets, response) {
             if (!error) {
                 for (tweet in tweets) {
                     console.log("You tweeted:", tweets[tweet].text, " on", tweets[tweet].created_at + "\n\n");
+                    fs.appendFile('log.txt', tweets[tweet].text+tweets[tweet].created_at, 'utf8', err =>{
+                        console.log('All done!');
+                    });
                 }
             }
         });
@@ -25,18 +26,25 @@ switch (input) {
         // https://api.spotify.com/v1/search?query=I+want+it+that+way&type=track&offset=0&limit=20
         const songName = process.argv[3];
         spotify.search({ type: 'track', query: "'" + songName + "'" }, function(err, data) {
-
-            if (!err) {
-                console.log("Song: " + songName);
-                console.log("Artist:", data.tracks.items[0].artists[0].name);
-                console.log("Album: ", data.tracks.items[0].album.name);
-                console.log("Preview link: " + data.tracks.items[0].preview_url);
-            } else {
-                console.log("Your song doesn't seem to be popping up! Want to listen to this one instead?");
+            if (songName === undefined) {
+                fs.appendFile('log.txt', 'They did not type in a song...', 'utf8', err => {
+                    console.log('All done!');
+                });
+                console.log("You didn't tell me a song...How about this one?");
                 console.log("Song: The Ace");
                 console.log("Artist: Ace of Base");
                 console.log("Album: The Sign (US Album)");
                 console.log("Preview link: https://p.scdn.co/mp3-preview/4c463359f67dd3546db7294d236dd0ae991882ff?cid=null");
+            } else if (!err){
+                console.log("Song: " + songName);
+                console.log("Artist:", data.tracks.items[0].artists[0].name);
+                console.log("Album: ", data.tracks.items[0].album.name);
+                console.log("Preview link: " + data.tracks.items[0].preview_url);
+                fs.appendFile('log.txt', songName+'\n'+data.tracks.items[0].artists[0].name+'\n'+data.tracks.items[0].album.name+'\n'+data.tracks.items[0].preview_url, 'utf8', err => {
+                    console.log('All done!');
+                });
+            } else {
+                console.log('err');
             }
         });
         break;
@@ -44,6 +52,9 @@ switch (input) {
     case "movie-this":
         const movieTitle = process.argv[3];
         if (movieTitle === undefined) {
+            fs.appendFile('log.txt', 'They did not type in a movie!', 'utf8', err => {
+                console.log('All done!');
+            });
             console.log("Looks like you didn't type in a movie!");
             console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/");
             console.log("It's on Netflix!!");
@@ -56,6 +67,9 @@ switch (input) {
             }
             omdbApi.get(params, function(err, data) {
                 if (!err) {
+                    fs.appendFile('log.txt', data.Title+'\n'+data.Year+'\n'+data.imdbRating+'\n'+data.Country+'\n'+data.Language+'\n'+data.Plot+'\n'+data.Actors, 'utf8', err => {
+                        console.log('All done!');
+                    });
                     console.log("Movie title: " + data.Title);
                     console.log("Year released: " + data.Year);
                     console.log("IMDB Rating: " + data.imdbRating);
@@ -77,11 +91,15 @@ switch (input) {
         break;
 
     case "do-what-it-says":
+        data.Actors
         fs.readFile('/Users/NoelParisi/Desktop/liri/liri-node-app/random.txt', "utf8", function(err, data) {
             var dataArr = data.split(',');
             const songName = dataArr[1];
             spotify.search({ type: 'track', query: "'" + songName + "'" }, function(err, data) {
                 if (!err) {
+                    fs.appendFile('log.txt', songName+'\n'+data.tracks.items[0].artists[0].name+'\n'+data.tracks.items[0].album.name+'\n'+data.tracks.items[0].preview_url, 'utf8', err => {
+                        console.log('All done!');
+                    });
                     console.log("Song: " + songName);
                     console.log("Artist:", data.tracks.items[0].artists[0].name);
                     console.log("Album: ", data.tracks.items[0].album.name);
